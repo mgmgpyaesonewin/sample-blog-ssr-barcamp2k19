609f1583090efebf94cad0d20cd3b2a3546f06ea
pyaesone <pyaesone@softhatch.com>
2019-01-26 06:51:39 +0630

    article and post added

---
+++ pages/post/_id.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <article id="post">
+      <my-news
+        :image="image"
+        :title="title"
+        :content="content"
+      />
+    </article>
+    <my-footer />
+  </div>
+</template>
+
+<script>
+import MyNews from '@/components/MyNews.vue'
+import MyFooter from '@/components/MyFooter.vue'
+import data from '@/config/data'
+
+export default {
+  components: {
+    'my-news': MyNews,
+    'my-footer': MyFooter
+  },
+  computed: {
+    image() {
+      return data.posts.posts[this.$route.params.id - 1].image
+    },
+    title() {
+      return data.posts.posts[this.$route.params.id - 1].title
+    },
+    content() {
+      return data.posts.posts[this.$route.params.id - 1].content
+    }
+  }
+}
+</script>
